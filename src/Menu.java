3423f61a0a84fbc52ca093edb79bb07c1472fb91
relaxmax <maxime.frieh@yahoo.fr>
2014-05-22 06:22:47 +0200

    commit de ce que j'ai pu faire ;-)

--- src/Menu.java
+++ src/Menu.java
@@ -6,6 +6,8 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
+
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JColorChooser;
 import javax.swing.JPanel;
@@ -23,46 +25,21 @@ public class Menu extends JPanel {
 		setPreferredSize(new Dimension(710,35));
 		setLayout(new GridLayout(1, 10, 39, 5));
 		
-		// Boutons selectionnant le forme a dessiner
-		bouton[0]=(Bouton)this.add(new Bouton(new ImageIcon("../image/retour.png"), "Retour"));
-		bouton[0].setBackground(new Color(255, 255, 255));
-		bouton[0].setToolTipText("Retour");
-		
-		bouton[1]=(Bouton)this.add(new Bouton(new ImageIcon("../image/avance.png"), "Refaire"));
-		bouton[1].setBackground(new Color(255, 255, 255));
-		bouton[1].setToolTipText("Refaire");
-		
-		bouton[2]=(Bouton)this.add(new Bouton(new ImageIcon("../image/Rectangle.png"), "Rectangle"));
-		bouton[2].setBackground(new Color(255, 255, 255));
-		bouton[2].setToolTipText("Dessiner un Rectangle");
-		
-		bouton[3]=(Bouton)this.add(new Bouton(new ImageIcon("../image/Cercle.png"), "Cercle"));
-		bouton[3].setBackground(new Color(255, 255, 255));
-		bouton[3].setToolTipText("Dessiner un Cercle");
-
-		bouton[4]=(Bouton)this.add(new Bouton(new ImageIcon("../image/Triangle.png"), "Triangle"));
-		bouton[4].setBackground(new Color(255, 255, 255));
-		bouton[4].setToolTipText("Dessiner un Triangle");
-
-		bouton[5]=(Bouton)this.add(new Bouton(new ImageIcon("../image/Poligone.png"), "Polygone"));
-		bouton[5].setBackground(new Color(255, 255, 255));
-		bouton[5].setToolTipText("Dessiner un Polygone");
-	
-		bouton[6]=(Bouton)this.add(new Bouton(new ImageIcon("../image/move-cursor.jpg"), "Move"));
-		bouton[6].setBackground(new Color(255, 255, 255));
-		bouton[6].setToolTipText("Deplacer");
-		
-		bouton[7]=(Bouton)this.add(new Bouton(new ImageIcon("../image/Cursor_Hand.png"), "Resize"));
-		bouton[7].setBackground(new Color(255, 255, 255));
-		bouton[7].setToolTipText("Redimensionner");
-		
-		
-		
+		//Définition d"un groupe de boutons radio pour ne sélectionner qu'une figure à la fois
+		ButtonGroup radio=new ButtonGroup(); 
+		String[] labelImage={"Retour","Refaire","Rectangle","Cercle","Triangle","Polygone","Move","Resize"};
+		String[] commentaire={"Retour","Refaire","Dessiner un Rectangle","Dessiner un Cercle","Dessiner un Triangle","Dessiner un Polygone","Deplacer","Redimensionner"};
+		for(int i=0;i<8;i++){
+			bouton[i]=(Bouton)this.add(new Bouton(new ImageIcon("../image/"+labelImage[i]+".png"), labelImage[i]));
+			bouton[i].setBackground(new Color(255, 255, 255));
+			bouton[i].setToolTipText(commentaire[i]);
+		}
+		for(int i=0;i<4;i++){
+			radio.add(bouton[i]);
+		}
 		bouton[8]=(Bouton)this.add(new Bouton());
 		bouton[8].setBackground(new Color(255, 255, 255));
 		
-		
-
 		ActionListener act = new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				String selection = ((Bouton)e.getSource()).getNom();
@@ -80,45 +57,49 @@ public class Menu extends JPanel {
 				switch (selection){
 					case "Retour":
 						System.out.println("Retour");
-						choix = 1;
+						choix = 0;
 						break;
 						
 					case "Refaire":
 						System.out.println("Refaire");
-						choix = 2;
+						choix = 1;
 						break;
 						
 					case "Rectangle":
 						System.out.println("Rectangle");
-						choix = 3;
+						bouton[2].isSelected();
+						choix = 2;
 						break;
 
 					case "Cercle":
 						System.out.println("Cercle");
-						choix = 4;
+						bouton[2].isSelected();
+						choix = 3;
 						break;
 						
 					case "Triangle":
 						System.out.println("Triangle");
-						choix = 5;
+						bouton[2].isSelected();
+						choix = 4;
 						break;
 
 					case "Polygone":
 						System.out.println("Polygone");
-						choix = 6;
+						bouton[2].isSelected();
+						choix = 5;
 						break;
 						
 					case "Move":
 						System.out.println("Move");
 						setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
-						choix = 7;
+						choix = 6;
 						deplace = true;
 						break;	
 						
 					case "Resize":
 						System.out.println("Resize");
 						setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-						choix = 8;
+						choix = 7;
 						redim = true;
 						break;
 						
